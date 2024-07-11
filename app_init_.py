b3e141efb072d7908ae8d7aa3d62039a6ef6b4d3
kingfoxfriend <105112698+kingfoxfriend@users.noreply.github.com>
2024-07-11 23:25:19 +0900

    dev

--- app_init_.py
+++ app_init_.py
@@ -6,17 +6,10 @@ from sqlalchemy.orm import sessionmaker
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-# create the extension
-db = SQLAlchemy()
 # create the app
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../onpuscores.db'
+db = SQLAlchemy(app)
 
-
-# エンジンを作成
-engine = create_engine('sqlite:///onpuscores.db')
-
-# セッションを作成
-Session = sessionmaker(bind=engine)
-session = Session()
 
 import rootes
