d68116913d20df07ee150ca4bc365438acb06ffa
yufoxda <147848207+yufoxda@users.noreply.github.com>
2024-07-11 23:03:52 +0900

    Revert "flask"

--- rootes.py
+++ rootes.py
@@ -1,13 +1,5 @@
-from app_init_ import app,db
-from database.createSQL import Book, Song, Author, PublishURL
-from flask import render_template, request
+from app_init_ import app
 
 @app.route("/")
-def home():
-  return render_template('home.html')
-
-@app.route("/searchbook/",methods = ["GET"])
-def searchbook():
-  query = request.args.get('query')
-  books = db.session.query(Book).filter(Book.title.contains(query)).all()
-  return render_template('search_results.html', books=books)
+def index():
+  return "Flaskマスターに俺はなる！"
